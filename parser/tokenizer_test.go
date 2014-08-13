c6d0b9c9b2496e12823c6ad624978980d08852fa
Matt Butcher <technosophos@gmail.com>
2014-08-13 09:17:44 -0600

    Test main loop and errors.

--- parser/tokenizer_test.go
+++ parser/tokenizer_test.go
@@ -3,7 +3,28 @@ package parser
 import (
 	"testing"
 	"strings"
+	"io"
 )
+
+func TestMainLoop(t *testing.T) {
+	r := strings.NewReader("    ")
+	l := new(ListenerFixture)
+	z := NewTokenizer(r, l)
+
+	// Just make sure we are advancing.
+	for z.lastErr == nil {
+		z.Next()
+	}
+
+	if l.err != io.EOF {
+		t.Errorf("Listener got error %s", l.err)
+	}
+
+	if z.lastErr != io.EOF {
+		t.Errorf("Got error that evaded the listener: %s", z.lastErr)
+	}
+
+}
 
 func TestStrings(t *testing.T) {
 
